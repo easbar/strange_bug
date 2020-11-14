10bfc1a3421edb9f4212d51405509f37973076c3
Peter <graphhopper@gmx.de>
2020-11-14 01:55:48 +0100

    apache http

--- src/main/java/com/graphhopper/bug/Client.java
+++ src/main/java/com/graphhopper/bug/Client.java
@@ -2,22 +2,26 @@ package com.graphhopper.bug;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.http.Header;
+import org.apache.http.client.entity.GzipCompressingEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class Client {
-    private static final Logger logger = LoggerFactory.getLogger(Client.class);
-    private static final OkHttpClient client = createClient();
     public static final int PORT = 8989;
     private static final int NUM_WORK_A_REQUESTS = 1000;
     private static final int NUM_WORK_B_REQUESTS = 1000;
-    private static final int NUM_INTERFERE_REQUESTS = 10;
+    private static final int NUM_INTERFERE_REQUESTS = 100;
+    private static final CloseableHttpClient client = HttpClients.createDefault();
 
     public static void main(String[] args) throws Exception {
         Thread t1 = new Thread(() -> work("A", NUM_WORK_A_REQUESTS));
@@ -30,38 +34,35 @@ public class Client {
 
         t1.join();
         t2.join();
-        logger.info("finished");
+        System.out.println("finished");
+        client.close();
     }
 
     private static void work(String type, int count) {
         try {
-            logger.info("Start sending work requests of type " + type);
+            System.out.println("Start sending work requests of type " + type);
             for (int i = 0; i < count; i++) {
                 if (i > 0 && i % 1000 == 0)
-                    logger.info("Sent " + i + " work requests of type " + type);
+                    System.out.println("Sent " + i + " work requests of type " + type);
                 sleep(System.currentTimeMillis() % 37);
                 sendWorkRequest(type);
             }
-            logger.info("Work request of type " + type + " are all fine");
+            System.out.println("Work request of type " + type + " are all fine");
         } catch (Bug bug) {
-            logger.error("BUG DETECTED", bug);
-        } catch (IOException ex) {
-            logger.error("Error when sending work requests of type: " + type, ex);
+            System.out.println("BUG DETECTED " + bug.getMessage());
         }
     }
 
-    private static void sendWorkRequest(String type) throws IOException {
-        Response rsp = null;
-        try {
-            Request.Builder reqBuilder = new Request.Builder().url("http://localhost:" + PORT + "/work");
-            reqBuilder.post(RequestBody.create("{\"type\":\"" + type + "\"}", MediaType.parse("application/json")));
-            rsp = Client.client.newCall(reqBuilder.build()).execute();
-            String header = rsp.header("endpoint", "none");
-            if ("none".equals(header))
+    private static void sendWorkRequest(String type) {
+        HttpPost post = new HttpPost("http://localhost:" + PORT + "/work");
+        post.setEntity(new GzipCompressingEntity(new StringEntity("{\"type\":\"" + type + "\"}", ContentType.APPLICATION_JSON)));
+        try (CloseableHttpResponse rsp = client.execute(post)) {
+            Header header = rsp.getFirstHeader("endpoint");
+            if (header == null)
                 throw new IllegalStateException("Missing header for response of work request");
-            if (!"work".equals(header))
-                throw new Bug("BUG: Wrong header! Expected: work, Given: " + header);
-            String str = rsp.body().string();
+            else if (!"work".equals(header.getValue()))
+                throw new Bug("BUG: Wrong header! Expected: work, Given: " + header.getValue());
+            String str = EntityUtils.toString(rsp.getEntity());
 //            Object jsonobj = new ObjectMapper().readValue(str, Object.class);
 //            new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(jsonobj);
             Map<String, Object> json = getJson(str);
@@ -71,50 +72,34 @@ public class Client {
             String rspType = (String) json.get("type");
             if (!rspType.equals(type))
                 throw new Bug("BUG: Wrong response type for work request! Expected: " + type + ", Given: " + rspType);
-        } finally {
-            if (rsp != null) rsp.body().close();
+        } catch (Exception ex) {
+            System.out.println("Problem for work " + ex.getMessage());
         }
     }
 
     private static void interfere(int count) {
         try {
-            logger.info("Start sending interfere requests");
+            System.out.println("Start sending interfere requests");
             for (int i = 0; i < count; i++) {
                 if (i > 0 && i % 10 == 0)
-                    logger.info("Sent " + i + " interfere requests");
+                    System.out.println("Sent " + i + " interfere requests");
                 sleep(150);
                 sendInterfereRequest();
             }
-            logger.info("Interfere requests all fine");
+            System.out.println("Interfere requests all fine");
         } catch (IOException ex) {
-            logger.error("Error when sending interfere requests");
+            System.out.println("Error when sending interfere requests");
         }
     }
 
     private static void sendInterfereRequest() throws IOException {
-        Response rsp = null;
-        try {
-            Request.Builder reqBuilder = new Request.Builder().url("http://localhost:" + PORT + "/interfere");
-            reqBuilder.post(RequestBody.create("<bomb>", MediaType.parse("application/json")));
-
-            rsp = client.newCall(reqBuilder.build()).execute();
-            if (rsp.code() != 400)
-                throw new IllegalStateException("Expected 400 response from /interfere, but got: " + rsp.code());
-        } finally {
-            if (rsp != null) rsp.body().close();
+        HttpPost post = new HttpPost("http://localhost:" + PORT + "/interfere");
+        post.setEntity(new GzipCompressingEntity(new StringEntity("<bomb>", ContentType.APPLICATION_XML)));
+        try (CloseableHttpResponse rsp = client.execute(post)) {
+            EntityUtils.consume(rsp.getEntity());
+            if (rsp.getStatusLine().getStatusCode() != 400)
+                System.out.println("Expected 400 response from /interfere, but got: " + rsp.getStatusLine().getStatusCode());
         }
-    }
-
-    private static OkHttpClient createClient() {
-        return new OkHttpClient.Builder().
-                connectTimeout(6, TimeUnit.SECONDS).
-                readTimeout(12, TimeUnit.SECONDS).
-                writeTimeout(20, TimeUnit.SECONDS).
-                // default is 5 connections and 5min
-//                        connectionPool(new ConnectionPool(100, 15, TimeUnit.MINUTES)).
-        build()
-                .newBuilder().addInterceptor(new GzipRequestInterceptor()).
-                        build();
     }
 
     static Map<String, Object> getJson(String str) {
